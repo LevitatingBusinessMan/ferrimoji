fa756b276ea756c3cc7c1556e97cf82b4ad5bb75
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-02-24 01:48:30 +0100

    update versions

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # Ferrimoji
 Script to turn automate the creation of Ferris emojis.
-Currently it almost works but there are some major issues with the memory hanlding of libvips.
+Currently it almost works but there are some major issues with the memory handling of libvips.
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use anyhow::{Result, Error};
 static SCALEFACTOR: f64 = 8.0;
 
 fn main() -> Result<(), Error> {   
-    let vips = VipsApp::new("Ferrimoji", false)?;
+    let vips = VipsApp::new("Ferrimoji", false).expect("Could not initiate libvips");
     let ferris = VipsImage::new_from_file("images/ferris.png")?;
 
     //let ferris = VipsImage::new_from_file("images/ferris.png")?;
 
     fs::create_dir_all("out")?;
 
+
     for emoji_path in fs::read_dir("images/emojis")? {
+
 
         /*
         I manually strip the svg because rendering it causes antialiasing to create colors we can't pick out later.
@@ -20,31 +22,27 @@ fn main() -> Result<(), Error> {
         */
 
         let emoji_path = emoji_path?.path();
+        //let emoji_path = "images/emojis/1f605.svg";
 
         let emoji_svg = fs::read_to_string(&emoji_path)?;
         let mut parts = emoji_svg.split(">").collect::<Vec<&str>>();
         
-        //Second part
-        parts.remove(1);
+        // Remove the yellow circle of the emoji
+        parts.retain(|&part| !part.contains("fill=\"#FFCC4D\""));
 
         let stripped_emoji = parts.join(">");
 
-        let emoji = ops::svgload_buffer_with_opts(stripped_emoji.as_bytes(),&ops::SvgloadBufferOptions{
-            scale: SCALEFACTOR,
+        // svgload_buffer_with_opts is currently segfaulting
+        // but svgload, svgload_with_opts and svgload_buffer do work 
+        let emoji = ops::svgload_buffer_with_opts(stripped_emoji.as_bytes(), &ops::SvgloadBufferOptions {
             dpi: 72.0,
+            scale: 1.0,
             unlimited: false,
             flags: ops::ForeignFlags::None,
             memory: false,
             access: ops::Access::Random,
-            fail: false
+            fail_on: ops::FailOn::None,
         })?;
-
-        /*
-            Add is not perfect here.
-            I can try a different operation.
-            Or I could use the emoji as a mask to first delete certain pixels from ferris.
-            Or I could except this bug and call it a feature as it does result in nice images.
-        */
 
         let ferris_length = ferris.get_width();
         let emoji_length = emoji.get_width(); // this is wrong, should be 36?
@@ -56,6 +54,13 @@ fn main() -> Result<(), Error> {
         
         let ferris_clone = VipsImage::image_copy_memory(ferris.clone())?;
 
+        /*
+            Add is not perfect here.
+            I can try a different operation.
+            Or I could use the emoji as a mask to first delete certain pixels from ferris.
+            Or I could except this bug and call it a feature as it does result in nice images.
+        */
+
         ops::draw_image_with_opts(&ferris_clone, &emoji, (500/2)-(SCALEFACTOR as i32*36 as i32/2), 65, &ops::DrawImageOptions{
             mode: ops::CombineMode::Add
         })?;
@@ -64,12 +69,12 @@ fn main() -> Result<(), Error> {
 
         println!("{}", emoji_path.file_name().unwrap().to_owned().to_string_lossy().replace("svg", "png"));
 
-        ferris_clone_resized.image_write_to_file("x.png");
+        //ferris_clone_resized.image_write_to_file("x.png");
 
         //ferris_clone.image_set_kill(true);
         //ferris_clone1.image_set_kill(true);
 
-        //ferris_clone.image_write_to_file(format!("out/{}",emoji_path.file_name().unwrap().to_owned().to_string_lossy().replace("svg", "png")).as_str())?;
+        ferris_clone.image_write_to_file(format!("out/{}",emoji_path.file_name().unwrap().to_owned().to_string_lossy().replace("svg", "png")).as_str())?;
     }
 
     Ok(())
